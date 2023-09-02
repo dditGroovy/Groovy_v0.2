59cc96d31708df373d1803ec6f7f96fc303f5d41
leeheyjin <amideveloper@naver.com>
2023-09-02 10:01:46 +0900

    타겟 그만 커밋하고싶어요

---
+++ src/main/java/kr/co/groovy/vo/VehicleVO.java
@@ -0,0 +1,15 @@
+package kr.co.groovy.vo;
+
+import java.util.Date;
+
+public class VehicleVO {
+    private String vhcleNo;
+    private String vhcleVhcty;
+    private int vhclePsncpa;
+    private String commonCodeHipassAsnAt;
+    private int vhcleResveNo;
+    private Date vhcleResveBeginTime;
+    private Date vhcleResveEndTime;
+    private String vhcleResveEmplId;
+    private String commonCodeResveAt;
+}
